11093768bb95c6aa18c4364a081ac767312c800c
pokerfacer270 <Swidjaja270@gmail.com>
2023-02-26 19:46:25 -0800

    Numbers can display

---
+++ PotentialUEFI/kernel/src/BasicRenderer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "math.h"
+#include "Framebuffer.h"
+#include "simpleFonts.h"
+
+class BasicRenderer {
+    public:
+    BasicRenderer(Framebuffer* TargetFramebuffer, PSF1_FONT* psf1_Font);
+    Point CursorPosition;
+    Framebuffer* TargetFramebuffer;
+    PSF1_FONT* PSF1_Font;
+    unsigned int Color;
+    void Print(const char* str);
+    void PutChar(char chr, unsigned int xOff, unsigned int yOff);
+
+};
