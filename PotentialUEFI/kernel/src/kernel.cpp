4936bc78d49c97a3399e20b5ee6a828e783ba11d
pokerfacer270 <Swidjaja270@gmail.com>
2023-02-28 22:01:04 -0800

    Memory Map Made

--- PotentialUEFI/kernel/src/kernel.cpp
+++ PotentialUEFI/kernel/src/kernel.cpp
@@ -1,21 +1,30 @@
 #include <stdint.h>
 #include "BasicRenderer.h"
 #include "cstr.h"
+#include "efiMemory.h"
 
-extern "C" void _start(Framebuffer* framebuffer, PSF1_FONT* psf1_font) {
-    BasicRenderer newRenderer = BasicRenderer(framebuffer, psf1_font);
-    newRenderer.Print(to_string((uint64_t)1234932));
-    newRenderer.CursorPosition = {0, 16};
-    newRenderer.Print(to_string((int64_t)-1234932));
-    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_string((double)-13.14));
-    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_hstring((uint64_t)0xF0));
-    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_hstring((uint32_t)0xF0));
-    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_hstring((uint16_t)0xF0));
-    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_hstring((uint8_t)0xF0));
+struct BootInfo{
+    Framebuffer* framebuffer;
+    PSF1_FONT* psf1_Font;
+    void* mMap;
+    uint64_t mMapSize, mMapDescSize;
+};
+
+extern "C" void _start(BootInfo* bootInfo) {
+    BasicRenderer newRenderer = BasicRenderer(bootInfo->framebuffer, bootInfo->psf1_Font);
+
+    uint64_t mMapEntries = bootInfo->mMapSize / bootInfo->mMapDescSize;
+
+    for(int i = 0; i < mMapEntries; i++) {
+        EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)((uint64_t)bootInfo->mMap + (i * bootInfo->mMapDescSize));
+        newRenderer.Print(EFI_MEMORY_TYPE_STRINGS[desc->type]);
+        newRenderer.Color = 0xffff00ff;
+        newRenderer.Print(" ");
+        newRenderer.Print(to_string(desc->numPages * 4096 / 1024));
+        newRenderer.Print(" KB");
+        newRenderer.Color = 0xffffffff;
+        newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
+    }
+
     return ;
 }
